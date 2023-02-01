fd5b76eb40596a26bf841ee900e98de39bb229f6
Harsh Sahu <harshsahu0499@gmail.com>
2023-02-01 23:30:33 +0100

    added Login.js

---
+++ src/pages/Login.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+//import { useHistory } from "react-router-dom";
+import "../styles/Login.css";
+
+ const Login = () => {
+   const [emailAddress, setEmailAddress] = useState("");
+   const [password, setPassword] = useState("");
+  // const history = useHistory();
+
+   const handleSubmit = async (event) => {
+     event.preventDefault();
+
+     try {
+     console.log("here inside try block");
+       const response = await fetch(`http://localhost:8080/api/v1/users/email/${emailAddress}`, {
+         method: "GET",
+       });
+
+       if (!response.ok) {
+         throw new Error("User not found");
+       }
+
+       const user = await response.json();
+       if (user.password !== password) {
+         throw new Error("Incorrect password");
+       }
+
+       console.log("Login successful");
+       // history.push("/reports");
+         } catch (error) {
+           console.error(error);
+         }
+   };
+
+   return (
+     <form onSubmit={handleSubmit}>
+     <div>
+     <div>
+       <label>
+         &nbsp;&nbsp; Email address:
+         &nbsp;&nbsp;&nbsp;
+         <input
+           type="email"
+           value={emailAddress}
+           onChange={(event) => setEmailAddress(event.target.value)}
+         />
+       </label>
+       </div>
+       <div>
+       <label>
+         &nbsp;&nbsp; Password:
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <input
+           type="password"
+           value={password}
+           onChange={(event) => setPassword(event.target.value)}
+         />
+       </label>
+       </div>
+       <div>
+       &nbsp;&nbsp; <button type="submit"> Login</button>
+        </div>
+        <div>
+
+       <label> &nbsp;&nbsp; don't have an account &nbsp;&nbsp;
+       </label> <button type="submit"> SignUp</button>
+       </div>
+       </div>
+     </form>
+   );
+ };
+
+ export default Login;
